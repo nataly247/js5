2437c759d0c717aae1c0b557f0fd9c1977869afd
nataly247 <nataly247@gmail.com>
2019-03-03 17:22:33 +0200

    expenses object corrected

--- hw1/script.js
+++ hw1/script.js
@@ -6,34 +6,23 @@ let howMuch1 = parseInt((prompt("Во сколько обойдется?", "")))
 let must2 = prompt("Введите обязательную статью расходов в этом месяце", "");
 let howMuch2 = parseInt((prompt("Во сколько обойдется?", "")));
 
-/*я не понимаю, как свoйствам обекта присваиваить значения переменных */
 
 let appData = {
 	budget: money,
 	timeData: time,
-	/*Записать ответы в объект expenses в формате: 
-    expenses: {
-    “ответ на первый вопрос” : “ответ на второй вопрос”
-    }  ???  */
-	//expenses: {},
-	//expenses[must1, must2] = howMuch1, howMuch2,  ???
-	/* не понимаю как пока */
-	expenses: {
-		/* и в каком месте видео показано как cвойству обьекта, 
-		который находится внутри другого обьекта, 
-		надо присваивать значение переменной? */
-		must1: howMuch1,
-		must2: howMuch2
-	},
+	expenses: {},
 	optionalExpenses: 0,
 	income: [],
 	savings: false
 };
 
+appData.expenses[must1] = howMuch1;
+appData.expenses[must2] = howMuch2;
+
 alert("Ваш бюджет на месяц: " + appData.budget);
 
-alert("Обязательные расходы: " + "br" + must1 + ": "
-	+ appData.expenses["must1"] + " и " + must2 + ": " + appData.expenses["must2"]);
+alert("Обязательные расходы: " + "br" + must1 + ": " +
+	appData.expenses.must1 + " и " + must2 + ": " + appData.expenses.must2);
 
 let oneDayBudget = (Number(appData.budget) - howMuch1 - howMuch2 - Number(appData.optionalExpenses)) / 30;
 alert("За вычетом этих расходов вы можете тратить: " + oneDayBudget + " в день.").toFixed(2);
