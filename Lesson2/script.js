8ffcf0818d44d142d2d9b3746521ceda2bf2c345
nataly247 <nataly247@gmail.com>
2019-03-03 17:24:12 +0200

    Lesson2

---
+++ Lesson2/script.js
@@ -0,0 +1,34 @@
+let money = parseInt(prompt("Ваш бюджет на месяц?", "")),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "YYYY-MM-DD");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: 0,
+    income: [],
+    savings: false
+};
+
+for (let i = 0; i < 2; i++) {
+    do {
+        let must = prompt("Введите обязательную статью расходов в этом месяце", ""),
+            howMuch = parseInt((prompt("Во сколько обойдется?", "")));
+
+        /* если пользователь нажимает отмена - тип данных null,
+        если ничего не внес - пустая строка ''; a.length - длина;
+        делаем проверки: */
+        if ((typeof (must)) === 'string' && (typeof (must)) != null && (typeof (howMuch)) != null &&
+            must != '' && howMuch != '' && must.length < 50) {
+            appData.expenses[must] = howMuch;
+        } else {
+            alert("Введены некорректные данные");
+            continue;
+        }
+    }
+    while (i == 2);
+};
+
+appData.moneyPerDay = appData.budget / 30;
+alert("Ежедневный бюджет: " + appData.moneyPerDay.toFixed(2));
+//alert(typeof(m));
