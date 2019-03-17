680ed8c19e785030f9364fc4e97065a18f0eedef
nataly247 <nataly247@gmail.com>
2019-03-18 01:10:33 +0200

    budget app get elements

---
+++ Lesson5/budget_app/src/js/main.js
@@ -0,0 +1,25 @@
+let startBtn = document.getElementById('start'),
+
+    budgetValue = document.getElementsByClassName('budget-value')[0],
+    dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
+    levelValue = document.getElementsByClassName('level-value')[0],
+    expensesValue = document.getElementsByClassName('expenses-value')[0],
+    optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+    incomeValue = document.getElementsByClassName('income-value')[0],
+    monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+    yearSavingsValue = document.getElementsByClassName('yearsavings-value')[0],
+
+    expensesItem = document.getElementsByClassName('expenses-item'),
+
+    expensesItemBtn = document.getElementsByTagName('expenses-item-btn'), //Утвердить
+    optionalExpensesBtn = document.getElementsByTagName('optionalexpenses-btn'), //Утвердить
+    countBudgetBtn = document.getElementsByTagName('count-budget-btn'), //Рассчитать
+
+    optionalExpensesItem = document.querySelectorAll('optionalexpenses-item'),
+    incomeItem = document.querySelector('choose-income'),
+    checkSavings = document.querySelector('#savings'),
+    chooseSum = document.querySelector('.choose-sum'), //('#sum') - (?)
+    choosePersent = document.querySelector('.choose-percent'), //('#persent')
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
