e231c9e47f8a8639173cbe8c66a9cbd6265a398f
nataly247 <nataly247@gmail.com>
2019-03-23 21:30:59 +0200

    lesson11

---
+++ Lesson11/lesson11.js
@@ -0,0 +1,12 @@
+let options = {
+    width: 1366,
+    height: 768,
+    bakground: 'red',
+    font: {
+        size: '16px',
+        color: '#fff'
+    }
+};
+
+//console.log(JSON.stringify(options));  //converts js value to JSON object
+console.log(JSON.parse(JSON.stringify(options)));
